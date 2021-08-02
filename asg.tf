4329ca9ea527851a72e2d53d955ad7bc82aa8947
nondatech88 <68757669+nondatech88@users.noreply.github.com>
2021-08-02 13:01:08 -0500

    Update asg.tf

--- asg.tf
+++ asg.tf
@@ -1,3 +1,7 @@
+// export AWS_ACCESS_KEY_ID=<your access key>
+// export AWS_SECRET_ACCESS_KEY=<your secret key>
+// export AWS_DEFAULT_REGION=<your region>
+  
 provider "aws" {}
 
 resource "aws_vpc" "my_vpc" {
